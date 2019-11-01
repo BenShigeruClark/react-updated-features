b436678451fe2208fa3c3b9abc7766700162f2c4
Ben <benclarkphoto@mac.com>
2019-10-31 23:38:08 -0700

    add notes context

--- src/context/notes-context.js
+++ src/context/notes-context.js
@@ -1,32 +1,5 @@
-import React, { useState, useContext } from 'react'
-import NotesContext from '../context/notes-context'
+import React from 'react'
 
-const AddNoteForm = () => {
-  const { dispatch } = useContext(NotesContext)
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+const NotesContext = React.createContext()
 
-  const addNote = (e) => {
-    e.preventDefault()
-    dispatch({
-      type: 'ADD_NOTE',
-      title,
-      body
-    })
-    setTitle('')
-    setBody('')
-  }
-
-  return (
-    <>
-      <p>Add note</p>
-      <form onSubmit={addNote}>
-        <input value={title} onChange={(e) => setTitle(e.target.value)}/>
-        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
-        <button>add note</button>
-      </form>
-    </>
-  )
-}
-
-export { AddNoteForm as default }
+export { NotesContext as default }
