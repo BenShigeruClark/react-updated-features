bc464e40c1bb05f140b75347fa750fe0ea269886
Ben <benclarkphoto@mac.com>
2019-11-03 18:25:07 -0800

    index file change

--- src/index.js
+++ src/index.js
@@ -1,77 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-
-const NoteApp = () => {
-  const notesData = JSON.parse(localStorage.getItem('notes'))
-  const [notes, setNotes] = useState(notesData || [])
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-
-  const addNote = (e) => {
-    e.preventDefault()
-    setNotes([
-      ...notes,
-      { title, body }
-    ])
-    setTitle('')
-    setBody('')
-  }
-
-const removeNote = (title) => {
-  setNotes(notes.filter((note) => note.title !== title)) 
-}
-
-useEffect(() => {
-  localStorage.setItem('notes', JSON.stringify(notes))
-})
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      {notes.map((note) => (
-        <div key={note.title}>
-          <h3>{note.title}</h3> 
-          <p>{note.body}</p>
-          <button onClick={() => removeNote(note.title)}>x</button> 
-        </div>
-      ))}
-
-      <p>add note</p>
-      <form onSubmit={addNote}>
-        <input value={title} onChange={(e) => setTitle(e.target.value)} />
-        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
-        <button>add note</button>
-      </form>
-    </div>
-  )
-}
-
-// {/* // { count: 0, name: '' } */}
-
-const App = (props) => { 
-  const [count, setCount] = useState(props.count);
-  const [text, setText] = useState('');
-
-  useEffect(() => {
-    console.log('this should only run once')
-  }, [])
-
-  useEffect(() => {
-    console.log('useEffect ran')
-    document.title = count
-  }, [count])
-
-  return (
-    <div>
-      <p>the current {text || 'count'} is {count}</p>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(props.count)}>reset</button>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <input value={text} onChange={(e) => setText(e.target.value)}/>
-    </div>
-  )
-} 
+import NoteApp from './components/NoteApp'
 
 ReactDOM.render(<NoteApp />, document.getElementById('root'));
 
